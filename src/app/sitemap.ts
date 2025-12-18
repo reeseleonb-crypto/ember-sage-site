96013f7b176538c40fea344eddb920039f082023
Reese <reeseleonb@gmail.com>
2025-12-17 22:04:15 -0500

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,10 @@
+﻿import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://sachetbags.com/",
+      lastModified: new Date(),
+    },
+  ];
+}
